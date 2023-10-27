5bcc8b77469d73c2f7701fced1ed0f36d06b190b
Adefemi Oseni <adefemigreat1995@gmail.com>
2023-10-27 12:56:38 +0100

    fix cors

--- main.py
+++ main.py
@@ -66,6 +66,8 @@ async def inquire(data: InquiryData):
             
     if not files:
         raise HTTPException(status_code=400, detail="You need to upload at least one PDF file")
+    
+    return {"files": files}
             
     full_text = ""
     for file in files:
